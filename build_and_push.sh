9d4b0ee1f7c1c32014e0d08110cf53d2c4f672d7
Jan Staněk <jan@jstanek.cz>
2018-08-17 23:59:56 +0200

    Update build_and_push.sh

--- build_and_push.sh
+++ build_and_push.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-docker build -t fmasa/lebeda:7.1 lebeda/7.1 --no-cache
-docker build -t fmasa/lebeda:7.1-ci lebeda/7.1-ci --no-cache
+docker build -t jstanek/lebeda:7.1 lebeda/7.1 --no-cache
+docker build -t jstanek/lebeda:7.1-ci lebeda/7.1-ci --no-cache
 
-docker push fmasa/lebeda
+docker push jstanek/lebeda
